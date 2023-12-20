765787341f53e72c506cdb289fb22ab1aa70287a
Shadin Shaif <webdev.shadin@gmail.com>
2023-12-20 23:12:51 +0600

    first commit

---
+++ functions.php
@@ -0,0 +1,72 @@
+<?php
+if (site_url() == "http://localhost/theme/module-1") {
+    define('VERSION', time());
+} else {
+    define('VERSION', wp_get_theme()->get('version'));
+}
+
+function launcher_theme_setup()
+{
+    add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
+    load_theme_textdomain('launcher');
+}
+add_action('after_setup_theme', 'launcher_theme_setup');
+
+function launcher_assets()
+{
+    wp_enqueue_style('launcher', get_stylesheet_uri(), null, VERSION);
+    wp_enqueue_style('animate-css', get_theme_file_uri('/assets/css/animate.css'));
+    wp_enqueue_style('icomoon-css', get_theme_file_uri('/assets/css/icomoon.css'));
+    wp_enqueue_style('bootstrap-css', get_theme_file_uri('/assets/css/bootstrap.css'));
+    wp_enqueue_style('custom-css', get_theme_file_uri('/assets/css/style.css'), null, VERSION);
+    //Enqueue Scripts
+    wp_enqueue_script('easing-js', get_theme_file_uri('assets/js/jquery.easing.1.3.js'), array('jquery'), '0.0.1', true);
+    wp_enqueue_script('bootstrap-min-js', get_theme_file_uri('/assets/js/bootstrap.min.js'), array('jquery'), null, true);
+    wp_enqueue_script('waypointsmin-js', get_theme_file_uri('/assets/js/jquery.waypoints.min.js'), array('jquery'), null, true);
+    wp_enqueue_script('simple-counter-js', get_theme_file_uri('/assets/js/simplyCountdown.js'), array('jquery'), null, true);
+    wp_enqueue_script('main-js', get_theme_file_uri('/assets/js/main.js'), array('jquery'), null, true);
+}
+add_action('wp_enqueue_scripts', 'launcher_assets');
+
+//Register Widgets
+function launcher_widgets()
+{
+    register_sidebar(
+        array(
+            'name'          => __('Footer Right', 'launcher'),
+            'id'            => 'footer-rihgt',
+            'description'   => __('widgets in this area will be shown on footer left', 'launcher'),
+        )
+    );
+    register_sidebar(
+        array(
+            'name'          => __('Footer Left', 'launcher'),
+            'id'            => 'footer-left',
+            'description'   => __('widgets in this area will be shown on footer right', 'launcher'),
+
+            'before_widget' => '<li id="%1s" class="text-right widget %2s">',
+            'after_widget'  => '</li>',
+            'before_title'  => '<ul id="fh5co-social">',
+            'after_title'   => '</ul>',
+        )
+    );
+}
+
+add_action('widgets_init', 'launcher_widgets');
+
+//add background on leftside
+function launcher_styles()
+{
+    if (is_page()) {
+        $launcher_feture_image = get_the_post_thumbnail_url(null, 'large');
+?>
+        <style>
+            .bg-img {
+                background-image: url(<?php echo $launcher_feture_image; ?>);
+            }
+        </style>
+<?php
+    }
+}
+add_action('wp_head', 'launcher_styles');
---
+++ page-template/launcher.php
@@ -0,0 +1,66 @@
+<?php
+/*
+*Template Name: Launcher 
+*/
+?>
+<?php get_header(); ?>
+<body <?php body_class(); ?>>
+    <div class="fh5co-loader"></div>
+
+    <aside id="fh5co-aside" role="sidebar" class="text-center bg-img">
+        <h1 id="fh5co-logo"><a href="<?php site_url(); ?> "><?php bloginfo('name'); ?></a></h1>
+    </aside>
+
+    <div id="fh5co-main-content">
+        <div class="dt js-dt">
+            <div class="dtc js-dtc">
+                <div class="simply-countdown-one animate-box" data-animate-effect="fadeInUp"></div>
+
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="row">
+                            <div class="col-lg-7">
+                                <div class="fh5co-intro animate-box">
+                                    <h2><?php the_title(); ?></h2>
+                                    <p><?php the_content(); ?></p>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-7 animate-box">
+                                <form action="#" id="fh5co-subscribe">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" placeholder="Enter your email">
+                                        <input type="submit" value="Send" class="btn btn-primary">
+                                        <p class="tip"><?php _e('Please enter your email address for early access.', 'launcher') ?></p>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div id="fh5co-footer">
+            <div class="row">
+                <div class="col-md-6">
+                    <?php 
+                        if(is_active_sidebar('footer-left')){
+                            dynamic_sidebar('footer-left');
+                        }
+                    ?>
+                </div>
+                <div class="col-md-6 fh5co-copyright">
+                        <?php
+                            if(is_active_sidebar('footer-rihgt')){
+                                dynamic_sidebar('footer-rihgt');
+                            }
+                        ?>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+
+<?php get_footer(); ?>
